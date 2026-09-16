df76010949bb7f96964124888831c33eb240a2fe
Aikbryan1 <aikhenomianbryan@gmail.com>
2026-09-16 14:57:33 +0100

    temp: deploy build

--- android/app/src/main/kotlin/com/aikbryan/skyclock/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.aikbryan.skyclock
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/city.dart
+++
@@ -1,7 +0,0 @@
-class City {
-  final String name;
-  final String country;
-  final String timezone; // e.g. "Africa/Lagos"
-
-  City({required this.name, required this.country, required this.timezone});
-}
